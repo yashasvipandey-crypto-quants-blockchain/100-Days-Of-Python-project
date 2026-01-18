774ff33f3bbce96ca08c10dce9289bc7cf89ccf8
Yashasvi Pandey <yashasvipandey8888@gmail.com>
2026-01-18 17:43:11 +0530

    First upload from terminal

---
+++ practice.py
@@ -0,0 +1,35 @@
+import random
+# import pracice
+
+# random_integer=random.randint(1,10)
+# print(random_integer)
+# print(pracice.xsos)
+# random_number=random.random()*10
+# print(random_number)
+# random_float=random.uniform(1,10)
+# print(random_float)
+# rand_int=random.randint(0,1)
+# print(rand_int)
+
+
+# if rand_int=="0":
+#     print("heads")
+# else:
+#     print("tails")
+# states=["hello","hi","yo","gg"]
+# print(states[0])
+# states.extend(["guy","new","you"])
+# print(states)
+
+# friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
+# friends_r=random.randint(0,4)
+# print(friends_r)
+# print(friends[friends_r])
+# print(random.choice(friends))
+
+# fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+# vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+# num=len(fruits)
+# print(num)
+# dirty_dozen = [fruits, vegetables]
+# print(dirty_dozen[1][1])
